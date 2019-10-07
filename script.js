47f7e4bc3e466b7558a1d2df3ff258e2a7003f86
zey3830 <56261624+zey3830@users.noreply.github.com>
2019-10-07 09:40:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Ze"
+let userName = "zey3830";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
